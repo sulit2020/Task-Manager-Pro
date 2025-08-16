7bcf110a742f195613a38cd69006f06b4f56a2ab
Abraham Sulit <afacebu.abraham@gmail.com>
2025-08-16 23:27:00 +0800

    add table when deploying

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import django
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
+try:
+    call_command("migrate",interactive=False)
+except Exception as e:
+    print(f"Migration failed: {e}")
 
 application = get_wsgi_application()
